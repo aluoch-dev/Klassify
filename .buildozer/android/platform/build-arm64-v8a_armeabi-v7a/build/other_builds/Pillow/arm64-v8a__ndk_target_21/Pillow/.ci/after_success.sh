f6eebed39aa146d3c9974712142c8694d7c1ba99
aluoch-dev <laurinealuoch7@gmail.com>
2024-10-31 11:48:10 +0300

    remove buildozer

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/Pillow/arm64-v8a__ndk_target_21/Pillow/.ci/after_success.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# gather the coverage data
-pip3 install codecov
-if [[ $MATRIX_DOCKER ]]; then
-  coverage xml --ignore-errors
-else
-  coverage xml
-fi
--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/Pillow/arm64-v8a__ndk_target_21/Pillow/.ci/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-python3 -c "from PIL import Image"
-
-python3 -bb -m pytest -v -x -W always --cov PIL --cov Tests --cov-report term Tests $REVERSE
